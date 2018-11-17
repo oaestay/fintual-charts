f7eba0076fb7bc2dd95c59f3d4529714defdee8f
oaestay <oaestay@uc.cl>
2018-11-17 17:40:16 -0300

    chore(): Install Highcharts

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HighchartsVue from 'highcharts-vue'
+import Highcharts from 'highcharts'
+import stockInit from 'highcharts/modules/stock'
+
+stockInit(Highcharts)
 
 Vue.config.productionTip = false
+Vue.use(HighchartsVue);
 
 new Vue({
   render: h => h(App),
