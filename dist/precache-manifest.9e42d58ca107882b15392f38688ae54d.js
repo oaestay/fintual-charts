c83872983e381814d03e9984fa857afcb81d2549
oaestay <oaestay@uc.cl>
2018-11-18 14:26:21 -0300

    fix(): Use relative paths

---
+++ dist/precache-manifest.9e42d58ca107882b15392f38688ae54d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "6993d6821ac2b53f880e",
+    "url": "js/chunk-vendors.0c0fbd2e.js"
+  },
+  {
+    "revision": "f83b9c81fbd1b8bd39cf",
+    "url": "js/app.ef201ee6.js"
+  },
+  {
+    "revision": "cfba7cd806964a7b7f609d40b9146e0b",
+    "url": "index.html"
+  },
+  {
+    "revision": "f83b9c81fbd1b8bd39cf",
+    "url": "css/app.bfccc371.css"
+  }
+];
