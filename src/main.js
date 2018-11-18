32a045eff53a7a4b28812aa2c8b87f8e25461ad9
oaestay <oaestay@uc.cl>
2018-11-18 17:41:08 -0300

    chore(): Install vue-router

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import HighchartsVue from 'highcharts-vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import HighchartsVue from 'highcharts-vue';
 import Highcharts from 'highcharts'
-import stockInit from 'highcharts/modules/stock'
-import './registerServiceWorker'
+import stockInit from 'highcharts/modules/stock';
+import './registerServiceWorker';
 
-stockInit(Highcharts)
+stockInit(Highcharts);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(HighchartsVue);
+Vue.use(VueRouter);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
