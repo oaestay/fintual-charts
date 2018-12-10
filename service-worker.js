d1b7f201c07bc65475e3dcfbe1c7f475849959d6
oaestay <oaestay@uc.cl>
2018-12-10 15:51:31 -0300

    chore(): Add build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.5780a6bb96fe5b72d1ba7408e6fad76f.js"
+  "precache-manifest.cea944680af0c12098ab052450c7d7a4.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "fintual-graphs"});
