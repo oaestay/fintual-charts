c8f6ed8db23522bae2ffbd456dd845b768457e23
oaestay <oaestay@uc.cl>
2018-12-10 00:50:30 -0300

    chore(): Release new build

---
+++ precache-manifest.587ae21705500bb9b4e84b7b04262416.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "8fdda2d2a11b22775890",
+    "url": "js/chunk-vendors.da4ef457.js"
+  },
+  {
+    "revision": "070f5388e0fa9409c12a",
+    "url": "js/app.81bb2bb9.js"
+  },
+  {
+    "revision": "c420812f6b4771a0337acee2c808391a",
+    "url": "index.html"
+  },
+  {
+    "revision": "070f5388e0fa9409c12a",
+    "url": "css/app.f5624212.css"
+  }
+];
