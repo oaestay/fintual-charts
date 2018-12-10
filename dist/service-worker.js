4348660c30b2c0e854a4301ce4a4cda6cd35368d
oaestay <oaestay@uc.cl>
2018-12-10 00:50:30 -0300

    chore(): Release new build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.9e42d58ca107882b15392f38688ae54d.js"
+  "precache-manifest.587ae21705500bb9b4e84b7b04262416.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "fintual-graphs"});
